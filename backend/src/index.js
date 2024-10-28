3e1c4483af4a676c4c2cd8158235ef0c65f72d65
sharad choudhary <126180911+sharad102003@users.noreply.github.com>
2024-10-28 22:21:55 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,12 +5,17 @@ const taskRoutes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-// Middleware
-app.use(cors());
-app.use(express.json()); // This middleware is crucial
+// CORS configuration
+const corsOptions = {
+  origin: 'https://6z5p7y-3000.csb.app',
+};
+
+app.use(cors(corsOptions));
+app.use(express.json()); // Parse incoming JSON requests
 app.use('/api', taskRoutes);
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
+
